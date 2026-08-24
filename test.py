cdb921e664a2550ed6c1da52462fd783bdb0f4be
Felix z <124050977+felix971@users.noreply.github.com>
2026-08-24 22:51:01 +1000

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-image = np.zeros((256, 256), dtype=np.float32)
-mask = np.zeros((256, 256), dtype=np.int64)
-print("image:", image.shape, image.ndim, image.size, image.dtype, image.nbytes)
-print("mask:", mask.shape, mask.ndim, mask.size, mask.dtype, mask.nbytes)
